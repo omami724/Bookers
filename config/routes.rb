088d607980cac34bf0535642cc7e2506c6aedd47
MamiKido <o73724@gmail.com>
2022-12-21 06:19:22 +0000

    bookers

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books/show'
+  get 'books/edit'
+  get '/' => 'homes#top'
+  root to: 'homes#top'
+  get 'books'=> 'books#index', as: 'index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
